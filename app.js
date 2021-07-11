b0c432d60f30fe1c24e706a2399d61de70985a49
Aleksandr Ponomarenko <79024231+Ponomaleks@users.noreply.github.com>
2021-07-11 21:04:56 +0300

    done all tasks
    features:

--- app.js
+++ app.js
@@ -64,39 +64,77 @@ const galleryItems = [
   },
 ];
 
-
 const refs = {
-  gallery: document.querySelector("ul.js-gallery"),
-  lightbox: document.querySelector(".js-lightbox"),
-  lightboxImage: document.querySelector("img.lightbox__image"),
-  closeLightboxBtn: document.querySelector(".lightbox .lightbox__button"),
+  gallery: document.querySelector('ul.js-gallery'),
+  lightbox: document.querySelector('.js-lightbox'),
+  lightboxImage: document.querySelector('img.lightbox__image'),
+  closeLightboxBtn: document.querySelector('.lightbox .lightbox__button'),
+  overlay: document.querySelector('div.lightbox__overlay'),
 };
 
-console.log(refs.closeLightboxBtn);
-const gallaryItemsElements = galleryItems.map(el => `<li  class="gallery__item"><a class="gallery__link" href=${el.original}><img
+// Переменная индекса текущего выбранного изображения
+let lightboxImageIdx;
+
+// ================== разметка галереи ======================
+const galleryItemsElements = galleryItems.map(
+  el => `<li  class="gallery__item"><a class="gallery__link" href=${el.original}><img
       class="gallery__image"
       src=${el.preview}
       data-source=${el.original}
       alt="${el.description}"
-    /></a></li>`)
+    /></a></li>`,
+);
 
-   console.dir(gallaryItemsElements)
-refs.gallery.insertAdjacentHTML('afterbegin', gallaryItemsElements.join(''))
-
-refs.galleryLink = document.querySelector("a .gallery__link")
-
-
-refs.gallery.addEventListener('click', (ev) => {
+refs.gallery.insertAdjacentHTML('afterbegin', galleryItemsElements.join(''));
+const imgArray = [...document.querySelectorAll('.gallery__image')];
+// ================== открытие модалки ======================
+refs.gallery.addEventListener('click', ev => {
   ev.preventDefault();
-  refs.lightbox.classList.add('is-open')
-  refs.lightboxImage.src = ev.target.dataset.source
-  refs.lightboxImage.alt = ev.target.alt
-  console.dir(ev.target.dataset.source)
-})
+  refs.lightbox.classList.add('is-open');
+  refs.lightboxImage.src = ev.target.dataset.source;
+  refs.lightboxImage.alt = ev.target.alt;
+  refs.closeLightboxBtn.addEventListener('click', closeLightBox);
+  refs.overlay.addEventListener('click', closeLightBox);
+  document.addEventListener('keydown', onGalleryKeyPress);
 
-refs.closeLightboxBtn.addEventListener('click', (ev) => {
-  refs.lightbox.classList.remove('is-open')
-  refs.lightboxImage.src = ''
-  refs.lightboxImage.alt = ''
-})
+  // Запись значения индекса текущего выбранного изображения
+  lightboxImageIdx = imgArray.indexOf(ev.target);
+});
 
+function closeLightBox() {
+  refs.lightbox.classList.remove('is-open');
+  refs.lightboxImage.src = '';
+  refs.lightboxImage.alt = '';
+  refs.closeLightboxBtn.removeEventListener('click', closeLightBox);
+  refs.overlay.removeEventListener('click', closeLightBox);
+  document.removeEventListener('keydown', onGalleryKeyPress);
+}
+
+function onGalleryKeyPress(ev) {
+  if (ev.code === 'Escape') {
+    closeLightBox();
+  }
+
+  galleryScroll(ev);
+}
+
+function galleryScroll(ev) {
+  if (ev.key === 'ArrowLeft') {
+    if (lightboxImageIdx > 0) {
+      lightboxImageIdx -= 1;
+    } else {
+      lightboxImageIdx = galleryItems.length - 1;
+    }
+    refs.lightboxImage.src = galleryItems[lightboxImageIdx].original;
+    refs.lightboxImage.alt = galleryItems[lightboxImageIdx].description;
+  }
+  if (ev.code === 'ArrowRight') {
+    if (lightboxImageIdx < galleryItems.length - 1) {
+      lightboxImageIdx += 1;
+    } else {
+      lightboxImageIdx = 0;
+    }
+    refs.lightboxImage.src = galleryItems[lightboxImageIdx].original;
+    refs.lightboxImage.alt = galleryItems[lightboxImageIdx].description;
+  }
+}
